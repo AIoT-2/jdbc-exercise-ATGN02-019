9efcca2de229d85d4af4a55f744e08ad482a8ed2
nink2458 <nink2458@gmail.com>
2025-01-23 20:55:36 +0900

    init

--- src/main/java/com/nhnacademy/jdbc/student/repository/impl/PreparedStatementStudentRepository.java
+++ src/main/java/com/nhnacademy/jdbc/student/repository/impl/PreparedStatementStudentRepository.java
@@ -6,24 +6,30 @@ import com.nhnacademy.jdbc.util.DbUtils;
 import lombok.extern.slf4j.Slf4j;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Slf4j
-public class StatementStudentRepository implements StudentRepository {
+public class PreparedStatementStudentRepository implements StudentRepository {
 
     @Override
     public int save(Student student) {
-        String query = String.format("INSERT INTO jdbc_students(id, name, gender, age, created_at) VALUES('%s', '%s', '%s', %d, '%s')",
-                student.getId(), student.getName(), student.getGender().toString(), student.getAge(), student.getCreatedAtToString());
+        String query = "INSERT INTO jdbc_students(id, name, gender, age, created_at) VALUES(?, ?, ?, ?, ?)";
 
         try (Connection connection = DbUtils.getConnection();
-             Statement statement = connection.createStatement()
+             PreparedStatement preparedStatement = connection.prepareStatement(query)
         ) {
-            return statement.executeUpdate(query);
+            preparedStatement.setString(1, student.getId());
+            preparedStatement.setString(2, student.getName());
+            preparedStatement.setString(3, student.getGender().toString());
+            preparedStatement.setInt(4, student.getAge());
+            preparedStatement.setTimestamp(5, Timestamp.valueOf(student.getCreatedAt()));
+
+            return preparedStatement.executeUpdate();
         } catch (SQLException e) {
             log.error("{}", e.getMessage(), e);
         }
@@ -32,12 +38,14 @@ public class StatementStudentRepository implements StudentRepository {
 
     @Override
     public Optional<Student> findById(String id) {
-        String query = String.format("SELECT * FROM jdbc_students WHERE id='%s'", id);
+        String query = "SELECT * FROM jdbc_students WHERE id=?";
 
         try (Connection connection = DbUtils.getConnection();
-             Statement statement = connection.createStatement();
-             ResultSet resultSet = statement.executeQuery(query)
+             PreparedStatement preparedStatement = connection.prepareStatement(query)
         ) {
+            preparedStatement.setString(1, id);
+            ResultSet resultSet = preparedStatement.getResultSet();
+
             Student student = null;
             while (resultSet.next()) {
                 String name = resultSet.getString("name");
@@ -56,13 +64,17 @@ public class StatementStudentRepository implements StudentRepository {
 
     @Override
     public int update(Student student) {
-        String query = String.format("UPDATE jdbc_students SET name='%s', gender='%s', age=%d WHERE id='%s'",
-                student.getName(), student.getGender().toString(), student.getAge(), student.getId());
+        String query = "UPDATE jdbc_students SET name=?, gender=?, age=? WHERE id=?";
 
         try (Connection connection = DbUtils.getConnection();
-             Statement statement = connection.createStatement()
+             PreparedStatement preparedStatement = connection.prepareStatement(query)
         ) {
-            return statement.executeUpdate(query);
+            preparedStatement.setString(1, student.getName());
+            preparedStatement.setString(2, student.getGender().toString());
+            preparedStatement.setInt(3, student.getAge());
+            preparedStatement.setString(4, student.getId());
+
+            return preparedStatement.executeUpdate();
         } catch (SQLException e) {
             log.error("{}", e.getMessage(), e);
         }
@@ -71,12 +83,14 @@ public class StatementStudentRepository implements StudentRepository {
 
     @Override
     public int deleteById(String id) {
-        String query = String.format("DELETE FROM jdbc_students WHERE id='%s'", id);
+        String query = "DELETE FROM jdbc_students WHERE id=?";
 
         try (Connection connection = DbUtils.getConnection();
-             Statement statement = connection.createStatement()
+             PreparedStatement preparedStatement = connection.prepareStatement(query);
         ) {
-            return statement.executeUpdate(query);
+            preparedStatement.setString(1, id);
+
+            return preparedStatement.executeUpdate();
         } catch (SQLException e) {
             log.error("{}", e.getMessage(), e);
         }
---
+++ src/test/java/com/nhnacademy/jdbc/student/repository/impl/PreparedStatementStudentRepositoryTest.java
@@ -0,0 +1,8 @@
+package com.nhnacademy.jdbc.student.repository.impl;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+class PreparedStatementStudentRepositoryTest {
+
+}
