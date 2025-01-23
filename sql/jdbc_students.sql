b94a590f9f295393be84cfc684828f62a4344305
nink2458 <nink2458@gmail.com>
2025-01-23 20:43:15 +0900

    commit

--- sql/jdbc_students.sql
+++ sql/jdbc_students.sql
@@ -8,3 +8,15 @@ CREATE TABLE `jdbc_students`
     `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='학생';
+
+/* install */
+INSERT INTO jdbc_students(id, name, gender, age, created_at) VALUES(?, ?, ?, ?, ?);
+
+/* select */
+SELECT * FROM jdbc_students WHERE id=?;
+
+/* update */
+UPDATE jdbc_students SET name=?, gender=?, age=? WHERE id=?;
+
+/* delete */
+DELETE FROM jdbc_students WHERE id=?;
