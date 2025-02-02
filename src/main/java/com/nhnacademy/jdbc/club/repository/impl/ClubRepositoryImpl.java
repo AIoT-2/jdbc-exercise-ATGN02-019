44ad1091c12342a791a7b51656f844424fccb5cc
nink2458 <nink2458@naver.com>
2025-02-02 18:43:55 +0900

    init

---
+++ src/main/java/com/nhnacademy/jdbc/club/repository/impl/ClubRepositoryImpl.java
@@ -0,0 +1,44 @@
+package com.nhnacademy.jdbc.club.repository.impl;
+
+import com.nhnacademy.jdbc.club.domain.Club;
+import com.nhnacademy.jdbc.club.repository.ClubRepository;
+
+import java.sql.Connection;
+import java.util.Optional;
+
+public class ClubRepositoryImpl implements ClubRepository {
+
+    @Override
+    public Optional<Club> findByClubId(Connection connection, String clubId) {
+        //todo#3 club 조회
+
+        return Optional.empty();
+    }
+
+    @Override
+    public int save(Connection connection, Club club) {
+        //todo#4 club 생성, executeUpdate() 결과를 반환
+        return 0;
+    }
+
+    @Override
+    public int update(Connection connection, Club club) {
+        //todo#5 club 수정, clubName을 수정합니다. executeUpdate()결과를 반환
+
+        return 0;
+    }
+
+    @Override
+    public int deleteByClubId(Connection connection, String clubId) {
+        //todo#6 club 삭제, executeUpdate()결과 반환
+
+        return 0;
+    }
+
+    @Override
+    public int countByClubId(Connection connection, String clubId) {
+        //todo#7 clubId에 해당하는 club의 count를 반환
+
+        return 0;
+    }
+}
